38da11d0a1479ca10926359903b0f0c3ad121d8c
FerG0373 <fer.dg.sc@gmail.com>
2025-06-11 09:20:10 -0300

    Método mostrarMenu.

--- src/com/aluracursos/conversormonedas/principal/Main.java
+++ src/com/aluracursos/conversormonedas/principal/Main.java
@@ -1,7 +1,15 @@
 package com.aluracursos.conversormonedas.principal;
 
+import com.aluracursos.conversormonedas.ui.Menu;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner inputTeclado = new Scanner(System.in);
+        int opcion = 0;
+
+        Menu menu = new Menu(inputTeclado, opcion);
+        menu.mostrarMenu();
     }
 }
--- src/com/aluracursos/conversormonedas/ui/Menu.java
+++ src/com/aluracursos/conversormonedas/ui/Menu.java
@@ -1,4 +1,28 @@
 package com.aluracursos.conversormonedas.ui;
 
+import java.util.Scanner;
+
 public class Menu {
+    private Scanner teclado;
+    private int opcion;
+
+    public Menu(Scanner teclado, int opcion) {
+        this.teclado = teclado;
+        this.opcion = opcion;
+    }
+
+    public void mostrarMenu() {
+        System.out.println("""
+            *** ¡Bienvenido(a) a su Conversor de Monedas! ***
+            
+            1) Dólar =>> Peso argentino.
+            2) Peso argentino =>> Dólar.
+            3) Dólar =>> Real brasileño.
+            4) Real brasileño =>> Dólar.
+            5) Dólar =>> Euro.
+            6) Euro =>> Dólar.
+            7) Salir.
+            """);
+        System.out.print(">> ");
+    }
 }
